03902b112c11848758414177fa711411223c7a29
Quan Zhou <quan.zhou@affirm.com>
2020-09-26 13:50:56 -0700

    adds employee module with models package

---
+++ employees/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Unicode, Date
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class EmployeeDirectory(Base):
+    __tablename__ = 'employee_directory'
+
+    eid = Column(Integer, primary_key=True)
+    name = Column(Unicode(255), nullable=False)
+    gender = Column(String(1), nullable=False)
+    hire_date = Column(Date, nullable=False)
+    department = Column(Unicode(255), nullable=False)
+    division = Column(Unicode(255), nullable=False)
+    jobcode = Column(Unicode(255), nullable=False)
+    manager = Column(Unicode(255), nullable=False)
+    performance = Column(Unicode(255), nullable=False)
+    annual_benefits_value = Column(Unicode(255), nullable=False)
